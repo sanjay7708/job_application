e4913ec414159301e00f5006c8399b282b232e30
sanjay7708 <sanjaysri1310@gmail.com>
2026-03-20 14:59:00 +0530

    write ci cd pipeline

--- frontend/src/Components/auth/Login.jsx
+++ frontend/src/Components/auth/Login.jsx
@@ -1,18 +1,45 @@
 import React, { useState } from 'react'
+import api from '../../api';
 
 export const Login = () => {
     const [user,setUser]=useState({
         username:'',
         password:''
     })
+
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setUser((prev)=>({
+            ...prev,
+            [name]:value
+        }));
+    }
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault()
+        
+        try{
+            const res=await api.post('auth/login/',user)
+            alert(res.data.message)
+        }
+        catch (error) {
+        alert(error.response.data['message']);
+        }
+        
+    }
   return (
     <>
         <div>
-            <form method='post'>
+            <form method='post' onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor="">Username</label>
-                    <input type="text" value={user.username} onChange={(e)=>setUser((prev)=>e.target.value)} />
+                    <label>Username</label>
+                    <input type="text" value={user.username} name='username' onChange={handleChange} className='border rounded'/>
                 </div>
+                <div>
+                    <label>Password</label>
+                    <input type="text" value={user.password} name='password' onChange={handleChange} className='border rounded'/>
+                </div>
+                <button type='submit' className='border'>Login</button>
             </form>
         </div>
     </>
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 const api=axios.create({
     baseURL:'http://127.0.0.1:8000/',
-    withCredentials:true
+    
 })
 export default api
